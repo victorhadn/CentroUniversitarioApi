47a51d8c3d1feaf903d516e2a7fd8d185ba23741
Victor Henrique Amaral <54120724+victorhadn@users.noreply.github.com>
2020-12-09 22:26:00 -0300

    Add files via upload

---
+++ Universidade/src/test/java/br/com/universidade/UniversidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.universidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
